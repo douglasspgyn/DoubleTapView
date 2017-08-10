1b3144af6e1db933d4bddc7fb316f76bc53a73aa
douglasspgyn <douglasoarespereira@gmail.com>
2017-08-10 10:44:39 -0300

    - Fix for low API Version;

--- app/src/main/java/douglasspgyn/com/github/doubletapimage/DoubleTapView.java
+++ app/src/main/java/douglasspgyn/com/github/doubletapimage/DoubleTapView.java
@@ -6,8 +6,6 @@ import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.GradientDrawable;
 import android.graphics.drawable.LayerDrawable;
-import android.os.Build;
-import android.support.annotation.RequiresApi;
 import android.support.v4.content.ContextCompat;
 import android.util.AttributeSet;
 import android.util.TypedValue;
@@ -34,46 +32,25 @@ public class DoubleTapView extends RelativeLayout {
     private Drawable animatedViewIcon;
     private int animatedViewMeasure;
 
-    public DoubleTapView(Context context) {
-        super(context);
-        getTypedArray(null);
-        init(context);
-    }
-
     public DoubleTapView(Context context, AttributeSet attrs) {
         super(context, attrs);
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.DoubleTapView);
-        getTypedArray(typedArray);
-        init(context);
-        typedArray.recycle();
-    }
-
-    public DoubleTapView(Context context, AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.DoubleTapView, defStyleAttr, 0);
-        getTypedArray(typedArray);
-        init(context);
-        typedArray.recycle();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    public DoubleTapView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
-        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.DoubleTapView, defStyleAttr, 0);
-        getTypedArray(typedArray);
-        init(context);
-        typedArray.recycle();
+        init(context, typedArray);
     }
 
     private void getTypedArray(TypedArray typedArray) {
         int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 56, getResources().getDisplayMetrics());
         animatedViewBackgroundColor = typedArray.getString(R.styleable.DoubleTapView_animatedViewBackgroundColor) != null ? typedArray.getString(R.styleable.DoubleTapView_animatedViewBackgroundColor) : "#" + Integer.toHexString(ContextCompat.getColor(getContext(), R.color.colorAccent));
-        animatedViewIcon = typedArray.getDrawable(R.styleable.DoubleTapView_animatedViewDrawable);
+        animatedViewIcon = typedArray.getDrawable(R.styleable.DoubleTapView_animatedViewDrawable) != null ? typedArray.getDrawable(R.styleable.DoubleTapView_animatedViewDrawable) : ContextCompat.getDrawable(context, R.drawable.transparent_view);
         animatedViewMeasure = (int) typedArray.getDimension(R.styleable.DoubleTapView_animatedViewMeasure, px);
+
+        typedArray.recycle();
     }
 
-    private void init(Context context) {
+    private void init(Context context, TypedArray typedArray) {
         this.context = context;
+        getTypedArray(typedArray);
+
         rootView = inflate(context, R.layout.double_view, this);
         animatedView = (ImageView) rootView.findViewById(R.id.animated_view);
 
